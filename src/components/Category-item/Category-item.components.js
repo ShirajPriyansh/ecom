7a1a653929da99959a5d106ed1d324f0b08eda5d
Priyansh Kumar <72461561+ShirajPriyansh@users.noreply.github.com>
2022-12-30 16:17:10 +0530

    ecom first commmit

---
+++ src/components/Category-item/Category-item.components.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Category-item.styles.scss';
+
+
+const Category = ({category}) => {
+    const {title, imageUrl} = category
+  return (
+    <div className="category-container">
+    <div
+      className="background-image"
+      style={{ backgroundImage: `url(${imageUrl})` }}
+    ></div>
+    <div className="category-body-container">
+      <h2>{title}</h2>
+      <p>Show Now</p>
+    </div>
+  </div>
+  )
+}
+
+export default Category
